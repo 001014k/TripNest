0affb3536e020ac00adb3d18af8393afa2f26347
Kmj <hm4854@gmail.com>
2024-07-23 23:22:03 +0900

    20240723 (파이어베이스 연동)

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBlulv-Yf6pIh05e9LxOxmITHtiQmh67Rs")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
