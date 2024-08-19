bd8b026d197b680df01aa866daee9513e7c36c29
Kmj <hm4854@gmail.com>
2024-08-19 10:34:49 +0900

    202408019 (마커 세부사항 페이지에 해당 마커 주소를 불러오기)

--- lib/markerdetail_page.dart
+++ lib/markerdetail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
 
 class MarkerDetailPage extends StatefulWidget {
   final Marker marker;
@@ -15,12 +16,43 @@ class _MarkerDetailPageState extends State<MarkerDetailPage> {
   late TextEditingController _titleController;
   late TextEditingController _snippetController;
   String? _selectedKeyword;
+  String? _address;
 
   @override
   void initState() {
     super.initState();
-    _titleController = TextEditingController(text: widget.marker.infoWindow.title);
-    _snippetController = TextEditingController(text: widget.marker.infoWindow.snippet);
+    _titleController =
+        TextEditingController(text: widget.marker.infoWindow.title);
+    _snippetController =
+        TextEditingController(text: widget.marker.infoWindow.snippet);
+
+    // 좌표로 부터 주소 가져오기
+    _getAddressFromCoordinates(
+        widget.marker.position.latitude, widget.marker.position.longitude);
+  }
+
+  Future<void> _getAddressFromCoordinates(
+      double latitude, double longitude) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+      if (placemarks.isNotEmpty) {
+        final placemark = placemarks.first;
+        setState(() {
+          _address =
+              '${placemark.country ?? ''} ${placemark.administrativeArea ?? ''} ${placemark.locality ?? ''} ${placemark.street ?? ''}';
+        });
+      } else {
+        setState(() {
+          _address = '주소를 찾을 수 없습니다';
+        });
+      }
+    } catch (e) {
+      print('Error getting address: $e');
+      setState(() {
+        _address = '주소를 가져오는 중 오류 발생';
+      });
+    }
   }
 
   @override
@@ -60,6 +92,10 @@ class _MarkerDetailPageState extends State<MarkerDetailPage> {
               decoration: InputDecoration(labelText: '설명'),
             ),
             SizedBox(height: 20),
+            _address != null
+                ? Text('주소: $_address',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
+                : CircularProgressIndicator(), // 주소를 로드 중일 때 로딩 표시
             ElevatedButton(
               onPressed: _saveMarker,
               child: Text('저장'),
