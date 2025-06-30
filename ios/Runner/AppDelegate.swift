6473b69c8274af629c55c49c351e5f47d088392d
001014k <126381487+001014k@users.noreply.github.com>
2025-07-01 08:44:11 +0900

    20250701 (에뮬레이터 및 실 기기에서 사용할수 있게 업데이트 된 파일)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBlulv-Yf6pIh05e9LxOxmITHtiQmh67Rs")
+    GMSServices.provideAPIKey("AIzaSyD0eTjvrMm1wYfZcHl0AHL-dwBcd-MlOKs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
