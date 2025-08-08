37eb94411b78fb5a6a8dd50c0ea3690bc369b9e6
001014k <126381487+001014k@users.noreply.github.com>
2025-08-08 18:20:21 +0900

    20250808 (리뷰 세부사항 페이지에 있는 리뷰 칸 모델 추가)

---
+++ lib/models/review_preview_model.dart
@@ -0,0 +1,12 @@
+// models/review_preview_model.dart
+class ReviewPreviewModel {
+  final String platform;
+  final String url;
+  final String logoAssetPath;
+
+  ReviewPreviewModel({
+    required this.platform,
+    required this.url,
+    required this.logoAssetPath,
+  });
+}
